8143ec97fc1d510d549283920f2993a58c199cac
benjchan <ben.chan@me.com>
2019-09-06 14:13:18 +0100

    snippit fix

--- site/snippets/block.php
+++ site/snippets/block.php
@@ -4,7 +4,7 @@
 		<h1 class="align--center"><?= $data->title() ?></h1>
 	</div>
 
-	<? foreach($data->children() as $subpage){
+	<?php foreach($data->children() as $subpage){
 	    snippet($subpage->snippet(), ['data' => $subpage]);
 	}
 	?>
